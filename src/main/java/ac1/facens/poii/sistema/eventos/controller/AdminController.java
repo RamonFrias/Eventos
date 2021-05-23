5611bf67a85dcae0fef22e9b3debd82926b08dac
Ramon Frias <ramonfrias2451@gmail.com>
2021-05-23 20:28:16 -0300

    b

--- src/main/java/ac1/facens/poii/sistema/eventos/controller/AdminController.java
+++ src/main/java/ac1/facens/poii/sistema/eventos/controller/AdminController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
